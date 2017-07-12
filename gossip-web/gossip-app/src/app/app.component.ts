36f408bac1d4f184fbf4a61c81087dc0751205e3
mehrpouya <h.mehrpooya@gmail.com>
2017-07-12 16:21:14 +0000

    trying to workout how to add gossip as a class. and how to think about it?
    Should it be a class and then we make an array of it when we have the db service?

--- gossip-web/gossip-app/src/app/app.component.ts
+++ gossip-web/gossip-app/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Gossip app';
+  body = 'gossip body';
+}
+
+export class Gossip {
+  title = 'Gossip title';
+  body = 'gossip body';
+  comments = [{user:'anon',comment:'blah'},{user:'anon2',comment:'blah2'}];
 }
